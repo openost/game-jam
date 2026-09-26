708b2595dc2de22cf4bed7cebe5d4c95bd500e4b
omega-800 <gshevoroshkin@gmail.com>
2026-09-26 15:57:24 +0200

    remove deploy/ & gh pages

--- deploy/ci-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Expects DOCKER_TAGS to be set.
-set -euxo pipefail
-
-docker build --file=./Dockerfile --tag=openhsr/gamejam .
-for tag in $DOCKER_TAGS; do
-    docker tag openhsr/gamejam "$tag"
-done
